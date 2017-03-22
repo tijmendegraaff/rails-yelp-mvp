eea38a97ed7c236e709824e657999c1fef237b28
Tijmen de Graaff <tijmen76@gmail.com>
2017-03-22 12:42:08 +0100

    added crud for restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,8 +1,40 @@
 class RestaurantsController < ApplicationController
+  before_action :load_restaurant, only: [:show, :edit, :update, :destroy]
+
   def index
     @restaurants = Restaurant.all
   end
 
   def show
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurants_path
+  end
+
+  def edit
+  end
+
+  def update
+    # @task.done = boolean_value_for_checkbox_inputs(task_params[:done])
+    @restaurant.update(restaurant_params)
+    redirect_to restaurant_path
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def load_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+     Restaurant.create(
+        name: Faker::Book.title
+        address: Address.street_address
+        phone_number: Faker::phone_number
+        category:
+      )
+end
+
+20.times do
+     Review.create(
+        content: Faker::Book.title
+        rating: Address.street_address
+      )
+end
